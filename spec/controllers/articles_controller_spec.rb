f20dfda0f4aa592b488c9f70e6082d58bf72c471
Diego Balseiro <dbalseiro@stackbuilders.com>
2018-06-23 23:37:29 -0500

    Added more tests

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -14,46 +14,68 @@ RSpec.describe ArticlesController, type: :controller do
     end
   end
 
-  describe 'GET #index' do
-    it 'assigns specific articles'
-    it 'renders the search template'
+  describe 'GET #search' do
+    it 'assigns specific articles' do
+      article = create :article
+      params = { location: 'Manhattan' }
+      get :search, params: params
+      expect(assigns(:articles)).to eq [article]
+    end
+
+    it 'renders the search template' do
+      get :search
+      expect(response).to render_template('search')
+    end
   end
 
   describe 'GET #show' do
-    it 'assigns the requested contact to @contact'
-    it 'renders the :show template'
+    it 'assigns the requested article to @article' do
+      article = create :article
+      get :show, params: { id: article }
+      expect(assigns(:article)).to eq article
+    end
+
+    it 'renders the :show template' do
+      article = create :article
+      get :show, params: { id: article }
+      expect(response).to render_template('show')
+    end
   end
 
   describe 'GET #new' do
-    it 'assigns a new Contact to @contact'
+    it 'assigns a new article to @article' do
+      get :new
+      expect(assigns(:article).title).to be nil
+    end
+
     it 'renders the :new template'
   end
 
   describe 'POST #create' do
     context 'with valid attributes' do
-      it 'saves the new contact in the database'
+      it 'saves the new article in the database'
       it 'redirects to the home page'
     end
 
     context 'with invalid attributes' do
-      it 'does not save the new contact in the database'
+      it 'does not save the new article in the database'
       it 're-renders the :new template'
     end
   end
 
   describe 'GET #edit' do
-    it "assigns a new Contact to @contact"
+    it 'assigns a new article to @article'
     it 'renders the :new template'
   end
 
   describe 'PUT #update' do
     context 'with valid attributes' do
-      it 'saves the new contact in the database'
+      it 'saves the new article in the database'
       it 'redirects to the home page'
     end
 
     context 'with invalid attributes' do
-      it 'does not save the new contact in the database'
+      it 'does not save the new article in the database'
       it 're-renders the :new template'
     end
   end
